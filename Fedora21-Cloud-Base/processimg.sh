dbf9f56abecb0aba5584e5f8844ea3ccc4b0676e
Dusty Mabe <dusty@dustymabe.com>
2014-11-26 03:56:10 +0000

    Add in license/author/purpose into script.

--- Fedora21-Cloud-Base/processimg.sh
+++ Fedora21-Cloud-Base/processimg.sh
@@ -1,7 +1,26 @@
-# This script will download a fedora image and then modify it 
-# to prepare it for Digital Ocean's infrastructure. It uses 
-# Docker to hopefully guarantee the behavior is consistent across 
-# different machines.
+#!/bin/bash
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free  Software Foundation; either version 2 of the License, or
+# (at your option)  any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA.
+#
+#
+# purpose: This script will download a fedora image and then modify it 
+#          to prepare it for Digital Ocean's infrastructure. It uses 
+#          Docker to hopefully guarantee the behavior is consistent across 
+#          different machines.
+#  author: Dusty Mabe (dusty@dustymabe.com)
 
 set -eux 
 mkdir -p /tmp/doimg/
