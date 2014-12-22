73be6c22ca75d58255093eec3e96a2821f05e1f9
Dusty Mabe <dusty@dustymabe.com>
2014-12-22 17:13:15 +0000

    DO has now decided that this image will only go to datacenters that have the metadata service and can provide vendor data to the instances. This means they would rather have a minimal cloud cfg in the image and provide most directives via vendor-data.

--- Fedora21-Cloud-Base/processimg.sh
+++ Fedora21-Cloud-Base/processimg.sh
@@ -40,7 +40,7 @@ DODATASOURCEURL='http://bazaar.launchpad.net/~cloud-init-dev/cloud-init/trunk/do
 export DODATASOURCEFILE='/usr/lib/python2.7/site-packages/cloudinit/sources/DataSourceDigitalOcean.py'
 
 # File location for DO cloud config
-export DOCLOUDCFGFILE='/etc/cloud/cloud.cfg.d/01_digitalocean.cfg'
+export DOCLOUDCFGFILE='/etc/cloud/cloud.cfg'
 
 # Create workdir and cd to it
 mkdir -p $TMPMNT && cd $WORKDIR
@@ -83,6 +83,7 @@ chroot $TMPMNT
 
 # Put in place the config from Digital Ocean
 cat << END > $DOCLOUDCFGFILE
+# Replace the fedora cloud.cfg with a minimal cloud.cfg from DO
 datasource_list: [ DigitalOcean, None ]
 datasource:
  DigitalOcean:
@@ -90,10 +91,6 @@ datasource:
    timeout: 10
 vendor_data:
    enabled: True
-disable_root: False
-ssh_pwauth: True
-growpart:
-   mode: off
 END
 
 # restore selinux permissions (from inside chroot)
