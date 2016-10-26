5e8d8fd5e5da4ba90b4bfec91a6cd485454907c5
Dusty Mabe <dusty@dustymabe.com>
2016-10-26 16:19:16 -0400

    DO-F25: Test installing new version of cloud-init package.

--- DigitalOcean/Fedora/Fedora25-Cloud-Base/processimg.sh
+++ DigitalOcean/Fedora/Fedora25-Cloud-Base/processimg.sh
@@ -22,23 +22,25 @@
 #          different machines.
 #  author: Dusty Mabe (dusty@dustymabe.com)
 
-set -eu 
+set -eux 
 mkdir -p /tmp/doimg/
-cd /tmp/doimg
 
-# need wget/sha256sum/unxz for this script
-for exe in wget sha256sum unxz; do
-    if ! which $exe >/dev/null; then
-        echo "You need $exe to run this program"
-        exit 1
-    fi
-done
+docker run -i --rm --privileged -v /tmp/doimg:/tmp/doimg fedora:24 bash << 'EOF'
+set -eux
+WORKDIR=/workdir
+TMPMNT=/workdir/tmp/mnt
 
 # Vars for the image
 XZIMGURL='https://kojipkgs.fedoraproject.org//work/tasks/7797/16177797/Fedora-Cloud-Base-25-20161023.n.0.x86_64.raw.xz'
 XZIMG=$(basename $XZIMGURL) # Just the file name
 XZIMGSUM='6785eb5ea26aa14bbf99bca2a5f8022234b715776ede1f530553725c84654541'
 IMG=${XZIMG:0:-3}           # Pull .xz off of the end
+
+# Create workdir and cd to it
+mkdir -p $TMPMNT && cd $WORKDIR
+
+# Get any additional rpms that we need
+dnf install -y gdisk wget xz
 
 # Get the xz image, verify, and decompress the contents
 wget $XZIMGURL
@@ -49,5 +51,43 @@ if [ "$imgsum" != "$XZIMGSUM" ]; then
 fi
 unxz $XZIMG
 
-echo "The following image has been downloaded verified and uncompressed:"
-echo "   $(pwd)/$XZIMG"
+# Find the starting byte and the total bytes in the 1st partition
+# NOTE: normally would be able to use partx/kpartx directly to loopmount
+#       the disk image and add the partitions, but inside of docker I found
+#       that wasn't working quite right so I resorted to this manual approach.
+PAIRS=$(partx --pairs $IMG)
+eval `echo "$PAIRS" | head -n 1 | sed 's/ /\n/g'`
+STARTBYTES=$((512*START))   # 512 bytes * the number of the start sector
+TOTALBYTES=$((512*SECTORS)) # 512 bytes * the number of sectors in the partition
+
+# Discover the next available loopback device
+LOOPDEV=$(losetup -f)
+LOMAJOR=''
+
+# Make the loopback device if it doesn't exist already
+if [ ! -e $LOOPDEV ]; then
+    LOMAJOR=${LOOPDEV#/dev/loop} # Get just the number
+    mknod -m660 $LOOPDEV b 7 $LOMAJOR
+fi
+
+# Loopmount the first partition of the device
+losetup -v --offset $STARTBYTES --sizelimit $TOTALBYTES $LOOPDEV $IMG
+
+# Mount it on $TMPMNT
+mount $LOOPDEV $TMPMNT
+
+wget -O ${TMPMNT}/foo.rpm https://kojipkgs.fedoraproject.org//packages/cloud-init/0.7.8/3.fc25/noarch/cloud-init-0.7.8-3.fc25.noarch.rpm
+chroot ${TMPMNT}/
+rpm -Uvh foo.rpm
+rm foo.rpm
+exit
+
+# umount and tear down loop device
+umount $TMPMNT
+losetup -d $LOOPDEV
+[ ! -z $LOMAJOR ] && rm -f $LOOPDEV #Only remove if we created it
+
+# finally, cp $IMG into /tmp/doimg/ on the host
+cp -a $IMG /tmp/doimg/ 
+
+EOF
